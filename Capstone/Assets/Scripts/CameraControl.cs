a8dc05419229193b221f1a070c19b04a1a3a3fde
eFlores824 <EduardoFlores824@gmail.com>
2015-10-08 13:07:19 -0600

    Lighting completed with first person camera

---
+++ Capstone/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+    public float sensitivity = 5.0f;
+    public GameObject left;
+    public GameObject Forward;
+
+    private float yRotation = 0.0f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float xRotation = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
+            yRotation += Input.GetAxis("Mouse Y") * sensitivity;
+            yRotation = Mathf.Clamp(yRotation, -360.0f, 360.0f);
+            transform.localEulerAngles = new Vector3(-yRotation, xRotation, 0);
+        }
+    }
+}
